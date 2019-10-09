07d6a92e75ae3d38412c63786ba95c3a49cc8149
Vanna Winland <vgwinland@gmail.com>
2019-10-09 21:44:07 +0000

    Automatically backed up by Learn

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -2,10 +2,10 @@ class BankAccount
 attr_reader :name 
 attr_accessor :balance, :status
 
-def initialize(name)
-  @name = name 
-  @status = "open"
-  @balance = "$1000"
+  def initialize(name)
+    @name = name 
+    @status = "open"
+    @balance = "$1000"
 end
 
 def 
